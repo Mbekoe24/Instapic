cd0c2b771630ce2f31a6dc88a41b53ed9d54a697
MBest24 <mbekoe24@gmail.com>
2020-05-13 12:22:40 -0400

    adding profile bio info/ img

--- photo-api/client/src/components/Profile.jsx
+++ photo-api/client/src/components/Profile.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Header from "./Header";
-
+import "./Profile.css";
 export default function Profile(props) {
   return (
     <div>
       <Header />
+      <div className="bio-container">
+        <img
+          className="profile-picture"
+          src="https://i.imgur.com/tsnpCYx.jpg"
+          alt="profile-picture"
+        />
+        <div className="user-info">
+          <h2>
+            Username <button>Edit Profile</button>
+          </h2>
+
+          <p>Bio</p>
+        </div>
+      </div>
+      {/* ////////////////// */}
+      {/* doesnt render anything it should render user photos */}
 
       {/* {props.photos.map((photo) => (
         <div>
@@ -18,7 +34,10 @@ export default function Profile(props) {
         </div>
       ))}
     </div> */}
-      {props.posts.map((post) => (
+
+      {/* ///////////// */}
+      {/* renders alll post  */}
+      {/* {props.posts.map((post) => (
         <div>
           {post.photos.map((photo) => (
             <div className="img-border">
@@ -30,7 +49,7 @@ export default function Profile(props) {
             </div>
           ))}
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
