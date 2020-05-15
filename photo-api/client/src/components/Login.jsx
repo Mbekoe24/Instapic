3c5c4047c9b9d63863fc55595b4ac0f0b1865670
MBest24 <mbekoe24@gmail.com>
2020-05-15 02:42:06 -0400

    update responsiveness for login

--- photo-api/client/src/components/Login.jsx
+++ photo-api/client/src/components/Login.jsx
@@ -18,55 +18,57 @@ export default class Login extends Component {
   render() {
     const { username, password } = this.state;
     return (
-      <div className="main-container">
-        <img className="iPhone" src="https://i.imgur.com/aGENvJd.png" />
-        <div className="login">
-          <form
-            className="login-form"
-            onSubmit={(e) => {
-              e.preventDefault();
-              this.props.handleLogin(this.state);
-              this.props.history.push("/home");
-            }}
-          >
-            <p className="login-title">InstaPic</p>
-            {/* <label htmlFor="username">username:</label> */}
-            <input
-              className="username-input"
-              placeholder="username"
-              id="username"
-              type="text"
-              name="username"
-              value={username}
-              onChange={this.handleChange}
-            />
-            <br />
+      <div className="login-container">
+        <div className="main-container">
+          <img className="iPhone" src="https://i.imgur.com/aGENvJd.png" />
+          <div className="login">
+            <form
+              className="login-form"
+              onSubmit={(e) => {
+                e.preventDefault();
+                this.props.handleLogin(this.state);
+                this.props.history.push("/home");
+              }}
+            >
+              <p className="login-title">InstaPic</p>
+              {/* <label htmlFor="username">username:</label> */}
+              <input
+                className="username-input"
+                placeholder="username"
+                id="username"
+                type="text"
+                name="username"
+                value={username}
+                onChange={this.handleChange}
+              />
+              <br />
 
-            <br />
+              <br />
 
-            <input
-              className="password-input"
-              placeholder="password"
-              id="password"
-              type="password"
-              name="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-            <br />
-            <button className="log-in-button">Log In</button>
+              <input
+                className="password-input"
+                placeholder="password"
+                id="password"
+                type="password"
+                name="password"
+                value={password}
+                onChange={this.handleChange}
+              />
+              <br />
+              <button className="log-in-button">Log In</button>
 
-            <hr className="sign-up-divider" />
+              <hr className="sign-up-divider" />
 
-            <div className="sign-up-container">
-              <p>
-                Don’t have an account?
-                <Link to="/signup">
-                  <span className="sign-up-title"> Sign Up</span>
-                </Link>
-              </p>
-            </div>
-          </form>
+              <div className="sign-up-container">
+                <p>
+                  Don’t have an account?
+                  <Link to="/signup">
+                    <span className="sign-up-title"> Sign Up</span>
+                  </Link>
+                </p>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
